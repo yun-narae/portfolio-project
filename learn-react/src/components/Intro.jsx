e91897961659579423621675cb6c88089c625c06
seulbinim <skfo0827@naver.com>
2025-01-14 16:57:42 +0900

    Intro 페이지 구현완료

--- learn-react/src/components/Intro.jsx
+++ learn-react/src/components/Intro.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
+import about from "../assets/img/aboutme.jpg";
+
 const Intro = () => {
-  return (
-    <div>Intro</div>
-  )
-}
+    return (
+        <section id="intro">
+            <div className="intro__inner">
+                <h1 className="intro__title">port developer<br/><span>scroll down ⬇️</span></h1>
+                
+                <div className="intro__text">
+                    <div className="text">
+                        <div>talent is</div>
+                        <div>found at the end of the</div>
+                        <div>effort</div>
+                    </div>
+                    <div className="img">
+                        <img src={about} alt="어바웃" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default Intro;
