2a647e26254c6e3c5c83f6a9c5125a96fedcdcb9
seulbinim <skfo0827@naver.com>
2025-01-16 09:21:43 +0900

    Skill 스타일링 수정 및 마무리

--- learn-react/src/components/Skill.jsx
+++ learn-react/src/components/Skill.jsx
@@ -23,8 +23,7 @@ const Skill = () => {
                 <div className="skill__desc">
                     {skillText.map((skill, key) => (
                         <div key={key}>
-                            <span>{key+1}.</span>
-                            <h3>{skill.title}</h3>
+                            <h3><span>{key+1}.</span>{skill.title}</h3>
                             <p>{skill.desc}</p>
                         </div>
                     ))}
